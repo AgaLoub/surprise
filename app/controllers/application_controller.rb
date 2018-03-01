9dbebe55af76b17e9d40e739dd6ad5c687c60725
Agathe Loubert <agaloubert@hotmail.com>
2018-03-01 09:33:50 +0100

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+
+  def blog
+  redirect_to "https://www.questcequisepasse.pw/blog#{request.fullpath.gsub('/blog','')}", :status => :moved_permanently
+end
 end
